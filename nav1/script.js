7285ceef84d98134de7c13f0d08aeddc668150e3
Who <blacklistuser420@gmail.com>
2022-09-27 13:22:26 +0000

    nav finish

--- nav1/script.js
+++ nav1/script.js
@@ -1,11 +1,15 @@
 const menuIcon = document.querySelector('.menuicon');
 const menuList = document.querySelector('.menulist');
+const menuLine = document.querySelectorAll('.line');
 menuIcon.addEventListener('click',()=>{
-    console.log(menuList.className);
     // menuList.style.height='340px'
     if(menuList.className !== 'menulist active'){
         menuList.classList.add('active');
         menuList.style.height='340px'
+        for (let i = 1; i < line.length; i++) {
+            const element = line[i];
+            element.classList.add(`lineactive${i}`)
+        }
     }else{
         menuList.classList.remove('active');
         menuList.style.height='0px'
